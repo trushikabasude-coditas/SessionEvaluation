baae7cd2c553b0a5fe8f2e0d2d0c81d62f0afdd0
trushikabasude-coditas <trushika.basude@coditas.com>
2026-02-17 13:08:47 +0530

    week1 evlauation

--- src/week1/Intern.java
+++ src/week1/Intern.java
@@ -1,15 +1,12 @@
 package week1;
 
 public class Intern extends SalaryCalculationSystem.Employee {
-
     public Intern(String name, double baseSalary, int attendanceDays, int rating) {
         super(name, baseSalary, attendanceDays, rating);
     }
-
     @Override
     double calculateGrossSalary() {
         double attendancePercent = (attendanceDays / 30.0) * 100;
-
         if (attendancePercent < 70) {
             return baseSalary - (baseSalary * 0.20);
         } else {
